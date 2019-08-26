82df305b77865a88ac0c954bc2b351d71740bce8
Drew Pepin <drewpepin1995@gmail.com>
2019-08-25 20:24:13 -0400

    adding sidebar/additional HTML pages

---
+++ javascript/javascript.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#sidebar").mCustomScrollbar({
+        theme: "minimal"
+    });
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+
+});
+
